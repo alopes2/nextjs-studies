d7f7cda18a8ea9bc4dd884fe5cd5fe2f440c4455
Andre Lopes <andrevitorlopes@gmail.com>
2024-06-02 22:26:18 +0200

    Add data fetch to react refreshner

--- react-refreshner/src/components/PostsList/PostsList.jsx
+++ react-refreshner/src/components/PostsList/PostsList.jsx
@@ -1,35 +1,65 @@
 import classes from './PostsList.module.css';
 
 import NewPost from '../NewPost/NewPost';
-import Post from '../Post/Post';
-import { useState } from 'react';
 import Modal from '../Modal/Modal';
+import Post from '../Post/Post';
+import { useEffect, useState } from 'react';
 
 const PostsList = ({ isPosting, onStopPosting }) => {
-  const [body, setBody] = useState('');
-  const [author, setAuthor] = useState('');
+  const [posts, setPosts] = useState([]);
+  const [isLoading, setLoading] = useState(false);
 
-  const bodyChangeHandler = (event) => {
-    setBody(event.target.value);
-  };
+  useEffect(() => {
+    (async function () {
+      setLoading(true);
+      const response = await fetch('http://localhost:8080/posts');
 
-  const authorChangeHandler = (event) => {
-    setAuthor(event.target.value);
+      const data = await response.json();
+
+      setPosts(data.posts);
+      setLoading(false);
+    })();
+  }, []);
+
+  const addPostHandler = async (postData) => {
+    const response = await fetch('http://localhost:8080/posts', {
+      method: 'POST',
+      body: JSON.stringify(postData),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    setPosts((prev) => [...prev, { ...postData }]);
   };
 
   return (
     <>
       {isPosting && (
         <Modal show={isPosting} onClose={onStopPosting}>
-          <NewPost
-            onBodyChange={bodyChangeHandler}
-            onAuthorChange={authorChangeHandler}
-          />
+          <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
         </Modal>
       )}
-      <ul className={classes.posts}>
-        <Post author={author} body={body} />
-      </ul>
+      {!isLoading && posts.length > 0 && (
+        <ul className={classes.posts}>
+          {posts.map((p, index) => (
+            <Post author={p.author} body={p.body} key={index} />
+          ))}
+        </ul>
+      )}
+
+      {!isLoading && posts.length === 0 && (
+        <div style={{ textAlign: 'center', color: 'white' }}>
+          <h2>There are no posts yet.</h2>
+          <p>Start adding some!</p>
+        </div>
+      )}
+
+      {isLoading && (
+        <div style={{ textAlign: 'center', color: 'white' }}>
+          <h2>Loading posts...</h2>
+        </div>
+      )}
     </>
   );
 };
